7d5af9f7dfd9bc7dac5462aeec74acc5735796d9
shreyasaithal <40474512+shreyasaithal@users.noreply.github.com>
2023-01-04 23:02:07 +0100

    adding header

--- app/routes/notes.tsx
+++ app/routes/notes.tsx
@@ -5,6 +5,7 @@ import type { Note } from "~/models/note.server";
 import { getNoteListItems } from "~/models/note.server";
 import { requireUserId } from "~/session.server";
 import { useUser } from "~/utils";
+import { Header } from "/";
 
 type LoaderData = {
   noteListItems: Note[];
@@ -55,25 +56,5 @@ export default function NotesPage() {
         </div>
       </main>
     </div>
-  );
-}
-
-function Header() {
-  const user = useUser();
-  return (
-    <header className="flex items-center justify-between bg-slate-800 p-4 text-white">
-      <h1 className="text-3xl font-bold">
-        <Link to=".">Notes</Link>
-      </h1>
-      <p>{user.email}</p>
-      <Form action="/logout" method="post">
-        <button
-          type="submit"
-          className="rounded bg-slate-600 py-2 px-4 text-blue-100 hover:bg-blue-500 active:bg-blue-600"
-        >
-          Logout
-        </button>
-      </Form>
-    </header>
   );
 }
