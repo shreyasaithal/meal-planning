faeea5197cbe12e4228f867554d18334e55965a7
shreyasaithal <40474512+shreyasaithal@users.noreply.github.com>
2023-01-04 23:25:55 +0100

    read7

--- app/routes/notes.tsx
+++ app/routes/notes.tsx
@@ -5,7 +5,7 @@ import type { Note } from "~/models/note.server";
 import { getNoteListItems } from "~/models/note.server";
 import { requireUserId } from "~/session.server";
 import { useUser } from "~/utils";
-import Header from "./shared/components/Header";
+import Header from "../shared/components/Header";
 
 type LoaderData = {
   noteListItems: Note[];
