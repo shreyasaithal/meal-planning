a4e725e336e7ca1b1372f3402233e59ea265f30a
shreyasaithal <40474512+shreyasaithal@users.noreply.github.com>
2023-01-09 22:14:07 +0100

    grid-test

--- app/routes/grid-test.tsx
+++ app/routes/grid-test.tsx
@@ -1,6 +1,23 @@
 import 'react-data-grid/lib/styles.css';
 import DataGrid from 'react-data-grid';
 
+import type { LoaderArgs } from "@remix-run/node";
+import { json } from "@remix-run/node";
+import { Form, Link, NavLink, Outlet, useLoaderData } from "@remix-run/react";
+import { requireUserId } from "~/session.server";
+import { useUser } from "~/utils";
+import Header from "../shared/components/Header";
+
+type LoaderData = {
+  noteListItems: Note[];
+};
+
+export async function loader ({ request }: LoaderArgs) {
+  const userId = await requireUserId(request);
+  const noteListItems = await getNoteListItems({ userId });
+  return json({ noteListItems });
+};
+
 const columns = [
   { key: 'id', name: 'ID' },
   { key: 'title', name: 'Title' }
@@ -11,6 +28,13 @@ const rows = [
   { id: 1, title: 'Demo' }
 ];
 
-function App() {
-  return <DataGrid columns={columns} rows={rows} />;
+export default function NotesPage() {
+  const data = useLoaderData<typeof loader>() as LoaderData;
+
+  return (
+    <div>
+      <Header />
+      <DataGrid columns={columns} rows={rows} />
+    </div>
+  );
 }
