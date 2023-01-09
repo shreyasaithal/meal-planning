7cb540d834258027263a88d5b5e2bb5d3aeafa80
shreyasaithal <40474512+shreyasaithal@users.noreply.github.com>
2023-01-10 00:27:39 +0100

    add divs

--- app/shared/components/Header/index.tsx
+++ app/shared/components/Header/index.tsx
@@ -5,19 +5,27 @@ import { Form, Link, NavLink, Outlet, useLoaderData } from "@remix-run/react";
 export default function Header() {
     const user = useUser();
     return (
-      <header className="flex items-left bg-slate-800 p-4 text-white">
-        <Link to="/meals">Meal Plan</Link>
-        <Link to="/ingredients">Ingredients</Link>
-
-        <p>{user.email}</p>
-        <Form action="/logout" method="post">
-          <button
-            type="submit"
-            className="rounded bg-slate-600 py-2 px-4 text-blue-100 hover:bg-blue-500 active:bg-blue-600"
-          >
-            Logout
-          </button>
-        </Form>
+      <header className="flex bg-slate-800 p-4 text-white">
+        <div class="grid grid-cols-6 gap-4">
+          <div>
+            <Link to="/meals">Meal Plan</Link>
+          </div>
+          <div>
+            <Link to="/ingredients">Ingredients</Link>
+          </div>
+          <div class="col-span-2">
+            <p>{user.email}</p>
+          </div>
+          <div class="col-span-2">
+            <Form action="/logout" method="post">
+            <button
+              type="submit"
+              className="rounded bg-slate-600 py-2 px-4 text-blue-100 hover:bg-blue-500 active:bg-blue-600"
+            >
+              Logout
+            </button>
+            </Form>
+          </div>
       </header>
     );
   }
