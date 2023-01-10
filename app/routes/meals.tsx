e18dd8cacd28d8f55bcbaf6efbe7b7784688b2cb
shreyasaithal <40474512+shreyasaithal@users.noreply.github.com>
2023-01-10 19:47:21 +0100

    width auto

--- app/routes/meals.tsx
+++ app/routes/meals.tsx
@@ -53,7 +53,7 @@ export default function Index() {
  return (
     <div>
         <Sidebar />
-        <section id="DataGrid" style={{ height: 550, width: '80%', marginLeft: 'auto', marginRight: 'auto' }}>
+        <section id="DataGrid" style={{ height: 550, width: 'auto', marginLeft: 'auto', marginRight: 'auto' }}>
             <DataGrid checkboxSelection={true} rows={rows} columns={columns} sx={{ m: 2 }}/>
         </section>
 
